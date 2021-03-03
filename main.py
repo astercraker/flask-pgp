b78008f14c7d0ce4e138f62d14b02f4c393f0a1c
LZTH532018 <luisgerardotf5@gmail.com>
2021-03-03 14:20:55 -0600

    Se muestra información de la base de datos

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 from model import db
 
 app = Flask(__name__)
-
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String, nullable=False)
+    fecha = db.Column(db.DateTime, default=datetime.now)
+    texto = db.Column(db.String, nullable=False)
 
 @app.route("/")
 def welcome():
-    return render_template("welcome.html", message="Mensage desde Jinga")
+    post = Post.query.order_by(Post.fecha.desc()).all()
+    return render_template("welcome.html", message="Mensaje desde Jinja!", post=post)
 
 
 @app.route("/card/<int:index>")
 def card_view(index):
     try:
-        card = db[index]
         return render_template("card.html", card=card)
     except IndexError:
         abort(404)
